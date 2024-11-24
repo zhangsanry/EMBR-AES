4642467aa6ef37b063f1fd47dba4d6a13073906b
zhangsanry <155344365+zhangsanry@users.noreply.github.com>
2024-11-24 19:26:50 +0800

    submit 18

--- read_embeddings.py
+++ read_embeddings.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-# 假设文件名为 example.npy，并且与你的脚本位于同一目录下
+
 file_path1 = 'embedding/embeddings_albert/train2.npy'
 
-# 加载 .npy 文件
+
 data1 = np.load(file_path1)
 
-# 输出数据
+
 print(data1.shape)
